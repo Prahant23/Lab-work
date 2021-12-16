a11dfa401fcf3724fa426e0d32c70972722b8c27
prashant23 <prashantbist64@gmail.com>
2021-12-16 09:06:38 +0545

    day5

---
+++ Day5/lab1.py
@@ -0,0 +1,6 @@
+for i in range(1500,2701):
+    if i%7==0:
+        print("divisible of 7 is",i)
+    elif i%5==0:
+        print("multipication of 5 is",i)
+    
---
+++ Day5/lab2.py
@@ -0,0 +1,11 @@
+# write a python programme to convert temperature to and from celsisus , fahrenheti.c = (5/9) * (f -32)3
+
+f = int(input("enter the temperature in farhenit:"))
+c = (5/9)*(f-32)
+print("the temperature in celsius {}".format(c))
+
+
+
+
+
+
